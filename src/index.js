b92dd26a7247b781929eaa0904bad892469c517f
jeshan <j@jeshan.co>
2017-06-25 14:01:52 +0400

    initial commit: from react slingshot. removed references to react

---
+++ src/index.js
@@ -0,0 +1,9 @@
+/* eslint-disable import/default */
+
+require('./favicon.ico'); // Tell webpack to load favicon.ico
+import './styles/styles.scss'; // Yep, that's right. You can import SASS/CSS files too! Webpack will run the associated loader and plug this into the page.
+
+if (module.hot) {
+  module.hot.accept('./bla/bla', () => {
+  });
+}
