0ccc636aceadfc5cfdb68e62bd6e3f8a3472507d
jeshan <j@jeshan.co>
2017-06-27 00:47:37 +0400

    informative message

--- tools/startMessage.js
+++ tools/startMessage.js
@@ -3,3 +3,4 @@ import {chalkSuccess} from './chalkConfig';
 /* eslint-disable no-console */
 
 console.log(chalkSuccess('Starting app in dev mode...'));
+console.log(chalkSuccess('Please close swagger editor window in the browser before stopping the "npm start" process'));
