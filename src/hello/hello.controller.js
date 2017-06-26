a5f95dd3a22ffac34e2566f6a4ce3a16dd44b919
jeshan <j@jeshan.co>
2017-06-27 00:47:15 +0400

    add example on how to use swagger-generated client side SDK

--- src/hello/hello.controller.js
+++ src/hello/hello.controller.js
@@ -1,9 +1,19 @@
 import angular from 'angular';
+// noinspection JSUnresolvedVariable
+import {DefaultApi} from '../../swagger-javascript-client';
 
 let app = angular.module('app');
+let api = new DefaultApi();
 
 app.controller('HelloCtrl', ['$scope', $scope => {
-  $scope.model = {
-    helloText: 'Hello my world'
-  };
+  $scope.model = {};
+
+  api.hello({}, (error, data, response) => {
+    if (error) {
+    } else {
+      let helloText = response.body;
+      $scope.model = {helloText};
+    }
+    $scope.$apply();
+  });
 }]);
