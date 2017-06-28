2a687ab9e4dcbdcbdd15c11f72dc327d30bb5000
jeshan <j@jeshan.co>
2017-06-29 00:42:59 +0400

    import demo html page

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@
 import './favicon.ico'; // Tell webpack to load favicon.ico
 import './styles/styles.scss'; // Yep, that's right. You can import SASS/CSS files too! Webpack will run the associated loader and plug this into the page.
 import './angular-app';
+import './hello/hello.html';
 import './hello/hello.controller';
 
 /*
