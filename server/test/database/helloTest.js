e80153fb8cae64ce3311d4b2f1f278156b306777
jeshan <j@jeshan.co>
2017-06-28 14:36:04 +0400

    moved config file

--- server/test/database/helloTest.js
+++ server/test/database/helloTest.js
@@ -1,5 +1,5 @@
 let mongoose = require('mongoose');
-let mongoUrl = require('../../config/constants/dev').mongoUrl;
+let mongoUrl = require('../../config/dev').mongoUrl;
 let expect = require('chai').expect;
 
 let HelloModel = require('../../config/database/models/hello');
